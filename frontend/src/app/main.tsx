e57d4ec6e9bc1f5e93b78bf65988da705e95d76e
Kajetan Rzepecki <k@spartan.works>
2018-01-21 23:32:12 +0100

    Added a stub of the frontend project.

---
+++ frontend/src/app/main.tsx
@@ -0,0 +1,20 @@
+import * as preact from "preact";
+import { MainContainer } from "./containers/main/mainContainer";
+import { mainStore } from "./store/main";
+
+export function onLoad() {
+  console.log("App successfully loaded!");
+  const container = document.createElement("div");
+  document.body.appendChild(container);
+  preact.render(<MainContainer store={mainStore}/>, container);
+}
+
+(function () {
+  const DOMContentLoaded = document.readyState === "interactive";
+
+  if (DOMContentLoaded) {
+    onLoad();
+  } else {
+    document.addEventListener("DOMContentLoaded", onLoad);
+  }
+})();
